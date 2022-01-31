af200c55d043ce299f2566349e402a04e2dba07b
throneee <89692376+throneee@users.noreply.github.com>
2022-01-31 23:56:59 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const handleFunction = () => {
+    document.querySelector('.app').innerHTML =
+        'Chúc bé iu năm mới vui vẻ, xinh đẹp, học giỏi và hạnh phúc bên gia đình cùng với người iu nhe. Iu bé nhìuuuu <33333';
+};
